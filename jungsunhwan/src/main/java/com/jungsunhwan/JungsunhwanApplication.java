47cd730e53a430899ccf45ac2ab4218d45d5b216
znzn9292 <znzn9292@gmail.com>
2024-07-25 11:54:55 +0900

    Add: Initial Commit

---
+++ jungsunhwan/src/main/java/com/jungsunhwan/JungsunhwanApplication.java
@@ -0,0 +1,13 @@
+package com.jungsunhwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungsunhwanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungsunhwanApplication.class, args);
+    }
+
+}
